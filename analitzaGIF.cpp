dca2090a85eb5e9de420d3c5fe69bef95e8714ec
Minepika55 <98884543+Minepika55@users.noreply.github.com>
2024-02-29 19:31:05 +0100

    Add files via upload

--- analitzaGIF.cpp
+++ analitzaGIF.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
+
 #include <fstream>
+
 #include <string>
+
 using namespace std;
 
 int main() {
-    int midaCapcalera = 13; 
+    int midaCapcalera = 13;
     char* capcalera = new char[midaCapcalera];
     string nom;
     unsigned int versio, ample, alcada, colors;
     char head1, head2, head3;
+    int duplicat;
 
     cout << "Analitzador de GIFs" << endl;
     cout << "Introdueix el nom del GIF (recorda posar .gif al final): ";
@@ -16,23 +20,22 @@ int main() {
 
     ifstream arxiu(nom.c_str(), ifstream::binary);
     if (arxiu.fail())
-        cout << "No s'ha detectat cap fitxer amb aquest nom" << endl;
+        cout << " ERROR no s'ha detectat cap fitxer amb aquest nom" << endl;
     else {
-        head1 = arxiu.get(); 
+        head1 = arxiu.get();
         head2 = arxiu.get();
         head3 = arxiu.get();
-        if ((head1 == 'G') && (head2 == 'I') && (head3 == 'F')) 
-        {
+        if ((head1 == 'G') && (head2 == 'I') && (head3 == 'F')) {
             cout << "Fitxer detectat com un gif: " << head1 << head2 << head3 << endl;
             arxiu.seekg(0, arxiu.beg);
             arxiu.read(capcalera, midaCapcalera);
             arxiu.close();
 
             char versioChar[4];
-            versioChar[0] = capcalera[3]; 
-            versioChar[1] = capcalera[4]; 
+            versioChar[0] = capcalera[3];
+            versioChar[1] = capcalera[4];
             versioChar[2] = capcalera[5];
-            versioChar[3] = '\0'; 
+            versioChar[3] = '\0';
 
             cout << "Versio del GIF: " << versioChar << endl;
 
@@ -45,8 +48,34 @@ int main() {
             colors = (unsigned char)capcalera[11] + (unsigned char)(capcalera[12]) * 256;
             cout << "Quantitat de colors del GIF: " << colors << endl;
         }
-        else 
-            cout << "Aquest fitxer no es un GIF" << endl;
+        else
+            cout << "ERROR aquest fitxer no es un GIF" << endl;
     }
+
+    cout << "Vols fer duplicar el GIF? 1.Si 2.No" << endl;
+    cin >> duplicat;
+    if (duplicat == 1) {
+
+        ifstream file(nom.c_str(), ios::binary | ios::ate);
+        streamsize size = file.tellg();
+        file.seekg(0, ios::beg);
+
+        char* copia = new char[size];
+
+        file.read(copia, size);
+        file.close();
+
+        string nouNom = nom.substr(0, nom.size() - 4) + "-2.gif";
+        ofstream nouGif(nouNom.c_str(), ios::binary);
+        nouGif.write(copia, size);
+        nouGif.close();
+
+        delete[] copia;
+        cout << "El fitxer s'ha duplicat sense problemes." << endl;
+    }
+    else {
+        cout << "ERROR no s'ha pogut duplicar el fitxer." << endl;
+    }
+
     return 0;
 }
